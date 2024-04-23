54d6fac0e509152d7a287d138c49ffdde2b59681
aldys002 <igidiaaldys@gmail.com>
2024-04-23 14:50:37 +0700

    membuat prosedur input

--- oop2.cpp
+++ oop2.cpp
@@ -3,4 +3,13 @@ using namespace std;
 
 class bangunDatar {
     
+    //akses modifier
+    private :
+        float panjang, lebar;
+    public :
+        float luas;
+
+        void input(){
+            
+        }
 }
